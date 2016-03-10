f8d88ee78262588e6f427183cedf120319d60b84
AliCollins <alistaircollins21@gmail.com>
2016-03-10 14:45:30 +0000

    Added project files.

---
+++ VNA_Data_Analysis/MainWindow.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Forms;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace VNA_Data_Analysis
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            // Retrieve InitialDirectory from Properties
+            folderTextBox.Text = Properties.Settings.Default.InitialDirectory;
+        }
+
+        /// <summary>
+        /// Update the selected data folder by using a FolderBrowserDialog.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void selectFolderButton_Click(object sender, RoutedEventArgs e)
+        {
+            var dialog = new System.Windows.Forms.FolderBrowserDialog();
+            dialog.SelectedPath = folderTextBox.Text;
+            System.Windows.Forms.DialogResult result = dialog.ShowDialog();
+            if(result == System.Windows.Forms.DialogResult.OK)
+            {
+                folderTextBox.Text = dialog.SelectedPath;
+                // Persist InitialDirectory by saving to a custom property
+                Properties.Settings.Default.InitialDirectory = dialog.SelectedPath;
+                Properties.Settings.Default.Save();
+            }
+            
+        }
+    }
+}
